9ed3066b21fe80babaeff3840764303ab8972455
Sean Oughton <seanoughton@gmail.com>
2018-06-28 17:28:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,8 +22,9 @@ class Route {
     //you have to find out how many blocks in streets(vertical) and how many blocks in ave's(horizontal) and add them together
     //you have to get the beginningLocation and end location for the route
     //you have to pull out the horz & vert for both beginning and ending
-    // for the vertical subtract gives distance
-    // for the horizontal use the array and test the index difference
+    // for the vertical subtract gives distance = vertical distance
+    // for the horizontal use the array and test the index difference = horizontal distance
+    // add vertical and horizontal and return
 
   }
 }
