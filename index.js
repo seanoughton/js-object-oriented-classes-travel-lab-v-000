dff8509a60c9e62e491f8550cdb13cba4ece38fa
Sean Oughton <seanoughton@gmail.com>
2018-06-28 17:32:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ class Route {
     //you have to get the beginningLocation and end location for the route
 
     // for the horizontal use the array and test the index difference = horizontal distance
-    horizontal =
+    horizontal =this.beginningLocation.horizontal this.endingLocation.horizontal
 
 
     vertical =this.beginningLocation.vertical - this.endingLocation.vertical;
