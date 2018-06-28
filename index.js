8612717663daf5fd067fcafddf0f6a397a390db5
Sean Oughton <seanoughton@gmail.com>
2018-06-28 17:59:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
+class Driver {
+  constructor(name,startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear();
+  }
+}
+
+class Route {
+  constructor(beginningLocation,endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+
+    let horizontal = 0;
+    let startAve = eastWest.indexOf(this.beginningLocation.horizontal);
+    let endAve = eastWest.indexOf(this.endingLocation.horizontal);
+
+    if (startAve === endAve) {
+      horizontal = 0;
+    } else {
+      horizontal = endAve - startAve;
+    };
+
+    let vertical = this.endingLocation.vertical - this.beginningLocation.vertical;
+
+    return vertical + horizontal;
+  }
+
+  estimatedTime(peak) {
+    let minutes = 0;
+    if (peak === true) {
+      minutes = this.blocksTravelled() / 2;
+    } else {
+      minutes = this.blocksTravelled() / 3;
+    }
+    return minutes
+  };
+
+}
