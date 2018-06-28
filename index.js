aac10f9688ecc89577ae4227987dc36455248d76
Sean Oughton <seanoughton@gmail.com>
2018-06-28 17:56:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ class Route {
     if (peak === true) {
       minutes = 0;
     } else {
-      minutes = this.blocksTravelled() * ;
+      minutes = this.blocksTravelled() / 3;
     }// peak hour
     return minutes
   };
