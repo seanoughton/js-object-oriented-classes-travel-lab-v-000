660deceb270e85c1a090a1ea4bca9947c74c3bed
Sean Oughton <seanoughton@gmail.com>
2018-06-28 17:54:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,6 +35,9 @@ class Route {
 
 //returns the number of minutes estimated for the trip. The estimated time depends on the distance in blocks and whether the trip is occurring during peak hours or off peak hours. During off peak hours, a driver travels three blocks in a minute, while during peak hours a driver travels an estimated two blocks in a minute.
   estimatedTime(peak) {
-
+    if (peak === true) {
+      
+    } // peak hour 
+    
   };
 }
