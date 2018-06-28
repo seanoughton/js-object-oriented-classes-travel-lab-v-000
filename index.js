693b2bc6fccbf64ff903ca42fa81b8fa47f2acc3
Sean Oughton <seanoughton@gmail.com>
2018-06-28 17:50:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,14 +17,14 @@ class Route {
 
   blocksTravelled() {
     let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
-    let horizontal = 0;
+    
     let startAve = eastWest.indexOf(this.beginningLocation.horizontal);
     let endAve = eastWest.indexOf(this.endingLocation.horizontal);
 
     if (startAve === endAve) {
-      horizontal = 0;
+      let horizontal = 0;
     } else {
-      horizontal = endAve - startAve;
+      let horizontal = endAve - startAve;
     };
 
     let vertical = this.endingLocation.vertical - this.beginningLocation.vertical;
