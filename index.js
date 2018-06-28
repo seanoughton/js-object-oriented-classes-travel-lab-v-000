60cd5637ac2375eda99c85e7498e1b698098c04c
Sean Oughton <seanoughton@gmail.com>
2018-06-28 17:24:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,5 +19,6 @@ class Route {
     //returns an integer with the number of blocks travelled
     let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
     //n New York City, to travel East and West you cross avenues, and to travel North and South you cross streets. To calculate the distance of in streets, you can use subtraction (eg. 13th street to 10th street is three blocks). To calculate the distance of avenues travelled, you will need to know the names and order of avenues in New York City. You can use the following array to help with your calculation:
+    //you have to find out how many blocks in streets and how many blocks in ave's and add them together
   }
 }
